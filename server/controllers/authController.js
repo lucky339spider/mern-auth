70cefd330e8980e6404860ab1b79df47f50d6340
lucky339spider <131868534+lucky339spider@users.noreply.github.com>
2025-06-12 12:09:06 +0200

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -27,9 +27,8 @@ export const register = async (req, res)=>{
 
         res.cookie('token', token, {
             httpOnly: true, 
-            secure: process.env.NODE_ENV ==='production', 
-            sameSite: process.env.NODE_ENV ==='production' ? 
-            'none' : 'strict', 
+            secure: true, 
+            sameSite: 'None', 
             maxAge: 7 * 24 * 60 * 60 * 1000
 
         });
